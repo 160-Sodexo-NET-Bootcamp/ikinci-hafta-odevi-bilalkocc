06814ba6d80ee08a3200cc85e932e857f698e46c
Bilal <bilal.kocc@gmail.com>
2021-12-29 19:48:43 +0300

    İnitialize commit

---
+++ ikinci-hafta-odevi-bilalkocc/ikinci-hafta-odevi-bilalkocc/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ikinci_hafta_odevi_bilalkocc
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
